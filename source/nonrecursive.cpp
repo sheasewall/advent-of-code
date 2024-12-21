bd54234cf4b65e193a49c9b6ded72205971c4f1b
sheasewall <sheasewall@gmail.com>
2024-12-20 20:01:14 -0500

    Additional Derived targetting

--- source/nonrecursive.cpp
+++ source/nonrecursive.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <typeindex>
 #include <unordered_map>
 
 
@@ -11,8 +12,12 @@ private:
 public:
     MyArray() : internal_arr() {}
     MyArray(std::array<X, D> arr) : internal_arr(arr) {}
+    MyArray(std::initializer_list<X> list) { std::copy(list.begin(), list.end(), internal_arr.begin()); }
+    MyArray(const X& fill) { std::fill(internal_arr.begin(), internal_arr.end(), fill); }
 
     void add(const MyArray<X, D, I>& arr2);
+    template <typename SignedX>
+    void add_signed(const MyArray<SignedX, D, I>& signed_arr);
     void scale(const X& scalar);
 
     I size() { return internal_arr.size(); }
@@ -25,6 +30,9 @@ public:
     X& operator[](const I& i);
     X getElement(const I& i) const;
     bool operator==(const MyArray<X, D, I>& arr);
+
+    template <typename Unsigned, typename Signed>
+    static MyArray<Unsigned, D, I> unsigned_add(const MyArray<Unsigned, D, I>& unsigned_arr, const MyArray<Signed, D, I>& signed_arr);
 };
 
 template <typename X, unsigned int D, typename I>
@@ -66,6 +74,24 @@ void MyArray<X, D, I>::scale(const X& scalar)
     {
         internal_arr[i] *= scalar;
     }
+}
+
+template<typename X, unsigned int D, typename I>
+template<typename SignedX>
+void MyArray<X, D, I>::add_signed(const MyArray<SignedX, D, I>& signed_arr)
+{
+    std::array<X, D> sum;
+    for (I i = 0; i < D; i++)
+    {
+        SignedX result = internal_arr[i] + signed_arr.getElement(i);
+        if (result < 0)
+        {
+            throw std::invalid_argument("Unsigned addition results in out of bounds coordinate");
+        }
+        X unsigned_result = static_cast<X>(result);
+        sum[i] = unsigned_result;
+    }
+    internal_arr = sum;
 }
 
 template <typename T, unsigned int Dim, typename Index>
@@ -274,7 +300,7 @@ template <typename BaseEntity, unsigned int Dim>
 class TileEntity
 {
     using Id = unsigned long;
-    using BaseEntityPtr = std::unique_ptr<BaseEntity>;
+    using BaseEntityPtr = std::shared_ptr<BaseEntity>;
     using Coords = MyArray<Id, Dim>;
 
     const Id id;
@@ -282,7 +308,9 @@ class TileEntity
     Coords coords;
 
 public:
-    TileEntity(Id i, BaseEntityPtr e, Coords c) : id(i), internal_entity(std::move(e)), coords(c) {}
+    virtual ~TileEntity() {}
+
+    TileEntity(Id i, BaseEntityPtr e, Coords c) : id(i), internal_entity(e), coords(c) {}
     Id getId() { return id; }
     BaseEntityPtr getInternalEntity() { return internal_entity; }
     Coords getCoords() { return coords; }
@@ -356,63 +384,87 @@ class TileEngine
     using WeakEntityPtr = std::weak_ptr<EngineEntity>;
 
     using Id = unsigned long;
-    using BaseEntityPtr = std::unique_ptr<BaseEntity>;
+    using BaseEntityPtr = std::shared_ptr<BaseEntity>;
     using Coords = MyArray<Id, Dim>;
     using Tile = std::unordered_map<Id, WeakEntityPtr>;
     using WeakTile = std::unordered_map<Id, WeakEntityPtr>;
     using World = TileGrid<BaseEntity, Dim>;
 
 private:
-    SharedEntityPtr createEntity(const BaseEntity& e, Coords loc);
+    SharedEntityPtr createEntity(const BaseEntityPtr& e_ptr, Coords loc);
 
 protected:
     World world;
     Id max_id;
     std::unordered_map<Id, SharedEntityPtr> entities;
+    std::unordered_map<std::type_index, std::unordered_map<Id, WeakEntityPtr>> type_registry;
 
 public:
     TileEngine() : world(), max_id(), entities() {}
     TileEngine(Coords widths) : world(widths), max_id(), entities() {}
 
-    Id spawn(const BaseEntity& new_entity, const Coords loc = std::vector<unsigned long>(Dim));
+    template<typename DerivedEntity>
+    Id spawn(const std::shared_ptr<DerivedEntity>& new_entity_ptr, const Coords loc = Coords(Id()));
+    template<typename DerivedEntity>
+    Id spawn(const DerivedEntity& new_entity, const Coords loc);
     void despawn(const Id& id);
 
-    void moveEntityBy(const Id id, Coords displacement);
+    void moveEntityBy(const Id id, MyArray<long, Dim> displacement);
     void moveEntityTo(const Id id, Coords new_coords);
 
     WeakEntityPtr getEntity(const Id id);
-    WeakTile getEntitiesAt(Coords coords) { return world.getTile(coords); }
+    template<typename DerivedEntity>
+    std::shared_ptr<DerivedEntity> getDerivedEntity(const Id id);
+    template<typename DerivedEntity>
+    std::weak_ptr<TileEntity<DerivedEntity, Dim>> getDerivedEngineEntity(const Id id);
+    std::unordered_map<Id, WeakEntityPtr> getEntitiesAt(Coords coords) { return world.getTile(coords); }
+    template <typename DerivedEntity>
+    std::vector<std::weak_ptr<DerivedEntity>> getEntitiesByType();
+    Id getNumberOfEntities() { return entities.size(); }
 };
 
-template <typename BaseEntity, unsigned int Dim>
-std::shared_ptr<TileEntity<BaseEntity, Dim>> TileEngine<BaseEntity, Dim>::createEntity(const BaseEntity& entity, Coords loc)
+template<typename BaseEntity, unsigned int Dim>
+std::shared_ptr<TileEntity<BaseEntity, Dim>> TileEngine<BaseEntity, Dim>::createEntity(const BaseEntityPtr& e_ptr, Coords loc)
 {
-    BaseEntityPtr base_ptr(new BaseEntity(entity));
-    return std::make_shared<EngineEntity>(max_id++, std::move(base_ptr), loc);
+    return std::make_shared<EngineEntity>(max_id++, e_ptr, loc);
 }
 
-template <typename BaseEntity, unsigned int Dim>
-typename TileEngine<BaseEntity, Dim>::Id TileEngine<BaseEntity, Dim>::spawn(const BaseEntity& new_entity, const Coords loc)
+template<typename BaseEntity, unsigned int Dim>
+template<typename DerivedEntity>
+typename TileEngine<BaseEntity, Dim>::Id TileEngine<BaseEntity, Dim>::spawn(const std::shared_ptr<DerivedEntity>& new_entity_ptr, const Coords loc)
 {
-    SharedEntityPtr e_ptr = createEntity(new_entity, loc);
+    SharedEntityPtr e_ptr = createEntity(new_entity_ptr, loc);
     Id id = e_ptr->getId();
     entities[id] = e_ptr;
+    type_registry[typeid(DerivedEntity)][id] = e_ptr;
     world.put(e_ptr);
     return id;
+}
+
+template<typename BaseEntity, unsigned int Dim>
+template<typename DerivedEntity>
+typename TileEngine<BaseEntity, Dim>::Id TileEngine<BaseEntity, Dim>::spawn(const DerivedEntity& new_entity, const Coords loc)
+{
+    std::shared_ptr<DerivedEntity> new_entity_ptr = std::make_shared<DerivedEntity>(new_entity);
+    return spawn(new_entity_ptr, loc);
 }
 
 template <typename BaseEntity, unsigned int Dim>
 void TileEngine<BaseEntity, Dim>::despawn(const Id& id)
 {
+    SharedEntityPtr e_ptr = entities[id];
     world.remove(entities[id]);
+    type_registry[typeid(e_ptr->getInternalEntity().get())].erase(id);
     entities.erase(id);
 }
 
 template <typename BaseEntity, unsigned int Dim>
-void TileEngine<BaseEntity, Dim>::moveEntityBy(const Id id, Coords displacement)
+void TileEngine<BaseEntity, Dim>::moveEntityBy(const Id id, MyArray<long, Dim> displacement) 
 {
-    displacement.add(entities[id]->getCoords());
-    moveEntityTo(id, displacement);
+    // Create a new coords array of the correct type
+    Coords current = entities[id]->getCoords();
+    current.add_signed(displacement);
+    moveEntityTo(id, current);
 }
 
 template <typename BaseEntity, unsigned int Dim>
@@ -434,7 +486,104 @@ std::weak_ptr<TileEntity<BaseEntity, Dim>> TileEngine<BaseEntity, Dim>::getEntit
     return entities[id];
 }
 
+template<typename BaseEntity, unsigned int Dim>
+template<typename DerivedEntity>
+std::shared_ptr<DerivedEntity> TileEngine<BaseEntity, Dim>::getDerivedEntity(const Id id)
+{
+    if (SharedEntityPtr e_ptr = getEntity(id).lock())
+    {
+        return std::dynamic_pointer_cast<DerivedEntity>(e_ptr->getInternalEntity());
+    }
+    return nullptr;
+}
+
+template<typename BaseEntity, unsigned int Dim>
+template<typename DerivedEntity>
+std::weak_ptr<TileEntity<DerivedEntity, Dim>> TileEngine<BaseEntity, Dim>::getDerivedEngineEntity(const Id id)
+{
+    if (SharedEntityPtr e_ptr = getEntity(id).lock())
+    {
+        return std::dynamic_pointer_cast<TileEntity<DerivedEntity, Dim>>(e_ptr);
+    }
+    return std::weak_ptr<TileEntity<DerivedEntity, Dim>>(); //Questionable
+}
+
+template<typename BaseEntity, unsigned int Dim>
+template<typename DerivedEntity>
+std::vector<std::weak_ptr<DerivedEntity>> TileEngine<BaseEntity, Dim>::getEntitiesByType()
+{
+    std::unordered_map<Id, WeakEntityPtr> base_e_ptrs = type_registry[typeid(DerivedEntity)];
+    std::vector<std::weak_ptr<DerivedEntity>> derived_e_ptrs;
+    for (auto& e_ptr : base_e_ptrs)
+    {
+        if  (SharedEntityPtr locked_e_ptr = e_ptr.second.lock())
+        {
+            auto derived_e_ptr = std::dynamic_pointer_cast<DerivedEntity>(locked_e_ptr->getInternalEntity());
+            derived_e_ptrs.push_back(derived_e_ptr);
+        }
+    }
+    return derived_e_ptrs;
+}
+
 ///
 ///
 ///
 
+class BaseEntity
+{
+public:
+    virtual ~BaseEntity() {}
+};
+
+class DerivedA : public BaseEntity
+{
+
+};
+
+class DerivedB : public BaseEntity
+{
+
+};
+
+class Engine2D : public TileEngine <BaseEntity, 2>
+{
+    using EngineEntity = TileEntity<BaseEntity, 2>;
+    using SharedEntityPtr = std::shared_ptr<EngineEntity>;
+    using WeakEntityPtr = std::weak_ptr<EngineEntity>;
+
+    using Id = unsigned long;
+    using BaseEntityPtr = std::shared_ptr<BaseEntity>;
+    using Coords = MyArray<Id, 2>;
+    using Tile = std::unordered_map<Id, WeakEntityPtr>;
+    using WeakTile = std::unordered_map<Id, WeakEntityPtr>;
+    using World = TileGrid<BaseEntity, 2>;
+
+    Id spawn(const BaseEntityPtr& new_entity_ptr, const Coords loc = Coords(0));
+
+
+// private:
+//     SharedEntityPtr createEntity(const BaseEntity& e, Coords loc);
+
+// protected:
+//     World world;
+//     Id max_id;
+//     std::unordered_map<Id, SharedEntityPtr> entities;
+
+// public:
+//     Engine2D() : world(), max_id(), entities() {}
+//     Engine2D(Coords widths) : world(widths), max_id(), entities() {}
+
+//     Id spawn(const BaseEntity& new_entity, const Coords loc = std::vector<unsigned long>(Dim));
+//     void despawn(const Id& id);
+
+//     void moveEntityBy(const Id id, Coords displacement);
+//     void moveEntityTo(const Id id, Coords new_coords);
+
+//     WeakEntityPtr getEntity(const Id id);
+//     WeakTile getEntitiesAt(Coords coords) { return world.getTile(coords); }
+};
+
+typename Engine2D::Id Engine2D::spawn(const BaseEntityPtr& new_entity_ptr, const Coords loc)
+{
+    return Id();
+}
